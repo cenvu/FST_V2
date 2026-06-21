a552abc89748dd0e6e09e76f75d8b569a1cfc9d3
CenVu <hunghv.hfs@gmail.com>
2026-06-21 22:01:02 +0700

    chore: restructure FST project layout and documentation

---
+++ FishSockTransfer/FishSockTransfer/FishSockTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishSockTransferApp.swift
+//  FishSockTransfer
+//
+//  Created by Cen Vũ on 15/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FishSockTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
